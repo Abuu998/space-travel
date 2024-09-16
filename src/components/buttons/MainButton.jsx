5094c07237b0c9782bb94ae98122de058801cbb6
unknown <abubakarndu@gmail.com>
2024-09-16 10:54:48 +0300

    feat: animated the home page

--- src/components/buttons/MainButton.jsx
+++ src/components/buttons/MainButton.jsx
@@ -1,15 +1,19 @@
+import { motion } from "framer-motion"
 
-
-function MainButton({ className, onClick=()=>{}, children, ...props }) {
+function MainButton({ className, onClick=()=>{}, variants, initial, animate, exit, children, ...props }) {
     return (
-        <button 
+        <motion.button
             className={`font-bellefair text-2xl bg-white p-12 aspect-square uppercase text-primary font-light rounded-full main-btn ${className}`}
             onClick={onClick}
             role="link"
+            variants={variants}
+            initial={initial}
+            animate={animate}
+            exit={exit}
             {...props}
         >
             {children}
-        </button>
+        </motion.button>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,62 @@ import MainHeading from "../components/text/MainHeading"
 import Heading5 from "../components/text/Heading5"
 import MainButton from "../components/buttons/MainButton"
 import { useNavigate } from "react-router-dom"
+import { AnimatePresence, motion } from "framer-motion"
+
+
+const containerVars = {
+    hidden: { opacity: 0, x: "-100vw" },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            x: {
+                ease: "easeInOut",
+                type: "spring",
+                stiffness: 100,
+                damping: 20,
+                delay: 0.2,
+            },
+            ease: "easeInOut",
+            duration: 0.5
+        }
+    },
+    exit: {
+        opacity: 0,
+        x: "-100vh",
+        transition: {
+            x: {
+                ease: "easeInOut",
+                type: "spring",
+                stiffness: 120,
+                damping: 20
+            }
+        }
+    }
+}
+
+
+const buttonVars = {
+    hidden: { opacity: 0, scale: 0 },
+    visible: {
+        opacity: 1,
+        scale: 1,
+        transition: {
+            ease: "easeInOut",
+            delay: 0.2,
+            duration: 0.5
+        }
+    },
+    exit: {
+        opacity: 0,
+        scale: 0,
+        transition: {
+            ease: "easeInOut",
+            duration: 0.5
+        }
+    }
+}
+
 
 
 function Home() {
@@ -11,15 +67,31 @@ function Home() {
     return (
         <section>
             <Container className="grid md:grid-cols-2 gap-16 sm:gap-28 sm:justify-center md:justify-items-center md:place-items-end">
-                <div className="text-center max-w-lg">
-                    <Heading5 className="">SO, YOU WANT TO TRAVEL TO</Heading5>
-                    <MainHeading className="mt-6 mb-4 text-6xl">space</MainHeading>
-                    <p className="text-lg">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
-                </div>
+                <AnimatePresence mode="wait">
+                    <motion.div 
+                        className="text-center max-w-lg"
+                        variants={containerVars}
+                        initial="hidden"
+                        animate="visible"
+                        exit="exit"
+                    >
+                        <Heading5 className="">SO, YOU WANT TO TRAVEL TO</Heading5>
+                        <MainHeading className="mt-6 mb-4 text-6xl">space</MainHeading>
+                        <p className="text-lg">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+                    </motion.div>
+                </AnimatePresence>
                 <div className="grid justify-center items-center max-w-lg lg:align-items-end">
-                    <MainButton onClick={() => navigate("/destination")}>
-                        Explore
-                    </MainButton>
+                    <AnimatePresence mode="wait">
+                        <MainButton 
+                            onClick={() => navigate("/destination")}
+                            variants={buttonVars}
+                            initial="hidden"
+                            animate="visible"
+                            exit="exit"
+                        >
+                            Explore
+                        </MainButton>
+                    </AnimatePresence>
                 </div>
             </Container>
         </section>
